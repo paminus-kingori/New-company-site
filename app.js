ae9c71164cf6fb7a0f329ce0d59d55b1824a90be
paminus-kingori <pkmurungi.pm@gmail.com>
2022-02-01 19:52:06 +0300

    new progress

--- app.js
+++ app.js
@@ -5,3 +5,10 @@
 // const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 // mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
+// mdc.textField.MDCTextField.attachTo(document.querySelector('.mdc-text-field')); 
+
+import {MDCTextField} from '@material/textfield';
+
+
+const mdcTF = require('@material/textfield');
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
--- js/nav.js
+++ js/nav.js
@@ -4,14 +4,24 @@ menuBtn.addEventListener("click",()=>{
     links.classList.toggle("show");
 })
 
+let header = document.querySelector(".nav");
+
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".nav").style.top = "0";
-  } else {
-    document.querySelector(".nav").style.top = "-60px";
+  if(currentScrollPos>672){
+    header.classList.add("addBg");
   }
+  else{
+    header.classList.remove("addBg");
+  }
+  // if (prevScrollpos > currentScrollPos) {
+  //   document.querySelector(".nav").style.top = "0";
+  // } else {
+  //   document.querySelector(".nav").style.top = "-60px";
+  // }
   prevScrollpos = currentScrollPos;
 }
+
+
